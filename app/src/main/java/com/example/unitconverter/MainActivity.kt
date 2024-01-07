93c77b122b02b243d55bb15f1f80c82ea1bf07b1
tahmid1118 <tahmidshahriar.bd@gmail.com>
2024-01-08 00:35:11 +0600

    UI design

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,16 +47,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column {
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
-            val context = LocalContext.current
-            Button(onClick = { Toast.makeText(context,
-                "Thanks for clicking", Toast.LENGTH_LONG).show() }) {
-                Text(text = "Click Me!")
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Milimeters")},
+                        onClick = { /*TODO*/ })
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Meters")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Feet")},
+                        onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text(text = "Milimeters")},
+                        onClick = { /*TODO*/ })
+                }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result:")
     }
     
